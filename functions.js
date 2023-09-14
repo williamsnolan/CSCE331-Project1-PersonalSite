03464e9264b150dfaeb62bb4d6e6b56c9610ddc9
Nolan Williams <williamsnolan03@gmail.com>
2023-09-14 16:33:23 -0500

    Added toggle button redid paragraphs added new contact info minor tweaks all around

---
+++ functions.js
@@ -0,0 +1,25 @@
+let currentCSS = localStorage.getItem("selectedCSS") || "mystyle.css"; // Use stored value or default
+
+// Function to change the CSS and store the choice in Local Storage
+function changeCSS() {
+    // Get the link element for the CSS file by its id
+    const cssLink = document.getElementById("cssLink");
+
+    // Toggle between CSS files
+    if (currentCSS === "mystyle.css") {
+        cssLink.href = "tronstyle.css";
+        currentCSS = "tronstyle.css";
+    } else {
+        cssLink.href = "mystyle.css";
+        currentCSS = "mystyle.css";
+    }
+
+    // Store the current CSS choice in Local Storage
+    localStorage.setItem("selectedCSS", currentCSS);
+}
+
+// Apply the stored CSS choice when the page loads
+window.addEventListener("load", () => {
+    const cssLink = document.getElementById("cssLink");
+    cssLink.href = currentCSS;
+});
